d13e016df0747c6580b0ead91e9c624210ee170c
ilkay <06ilkayozturk@gmail.com>
2023-10-05 23:54:57 +0300

    app:events

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ const rockImg = document.getElementById("rock");
 const paperImg = document.getElementById("paper");
 const scissorImg = document.getElementById("scissor");
 
+const yourChoiceDiv = document.getElementById("your-choice");
+
 // Variables
 
 // Event Listeners
+rockImg.addEventListener("click", () => {});
 
 // Functions
