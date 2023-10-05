a276ce8e822bc5fcfb3681b2f47022cb52b32a31
ilkay <06ilkayozturk@gmail.com>
2023-10-05 23:51:53 +0300

    app:selectors

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 // Selectors
 
+const rockImg = document.getElementById("rock");
+const paperImg = document.getElementById("paper");
+const scissorImg = document.getElementById("scissor");
+
 // Variables
 
 // Event Listeners
