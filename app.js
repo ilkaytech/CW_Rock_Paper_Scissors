2ab4f1bf9036fed4362e8e9bc979f80dc501722e
ilkay <06ilkayozturk@gmail.com>
2023-10-07 10:23:21 +0300

    app:rockImg:click

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 // Selectors
 
+//? Resimler
 const rockImg = document.getElementById("rock");
 const paperImg = document.getElementById("paper");
 const scissorImg = document.getElementById("scissor");
 
+//? Seçilen Eleman Taşıyıcıları
 const yourChoiceDiv = document.getElementById("your-choice");
 
 // Variables
+let image = document.createElement("img");
 
 // Event Listeners
-rockImg.addEventListener("click", () => {});
+rockImg.addEventListener("click", () => {
+  image.src = "./assets/rock.png";
+  image.alt = "rock";
+  yourChoiceDiv.appendChild(image);
+
+  //   yourChoiceDiv.innerHTML = `<img src"./assets/rock.png" alt="rock">`;
+});
 
 // Functions
